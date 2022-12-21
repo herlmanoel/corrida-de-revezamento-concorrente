b1e4b2718b061cc0a7dc12d194a6d8f150f87d87
feodpv03129 <hbarbosa@parceiros.odontoprev.com.br>
2022-12-21 17:05:12 -0300

    ajustes

--- main.go
+++ main.go
@@ -3,32 +3,58 @@ package main
 import (
 	"fmt"
 	"math/rand"
-	"strconv"
 	"sync"
 	"time"
 )
 
-func receiveBaton(waitGroup *sync.WaitGroup, baton chan string, id int) {
-	timeSleeping := time.Duration(rand.Int31n(1000)) * time.Millisecond
-	id = id + 1
-	isLast := (id + 1) > 4
-	person := "Runner " + strconv.Itoa(id)
-	baton <- person
+func run(waitGroup *sync.WaitGroup, baton chan int, team string) {
+	id := <-baton
+	timeSleeping := time.Duration(50+rand.Int31n(100)) * time.Millisecond * 10
+	fmt.Printf("[Team %s] Runner No.%d took the baton\n", team, id)
 	time.Sleep(timeSleeping)
-	fmt.Println("The "+person+" with the baton. TIME = ", timeSleeping)
-	if isLast {
-		fmt.Println("The race is over.")
-		waitGroup.Done()
-		return
+	if id < 4 {
+		fmt.Printf("[Team %s] Runner No.%d passed the baton. TIME = %dms\n", team, id, timeSleeping.Milliseconds())
+	} else {
+		fmt.Printf("[Team %s] Runner No.%d finished the race. TIME = %dms\n", team, id, timeSleeping.Milliseconds())
 	}
+
+	baton <- id + 1
 	waitGroup.Done()
-	go receiveBaton(waitGroup, baton, id)
+}
+
+func maketeam(teamWaitGroup *sync.WaitGroup, pole chan<- string, team string) {
+	const runners int = 4
+
+	baton := make(chan int, 1)
+	baton <- 1
+	var waitGroup sync.WaitGroup
+	waitGroup.Add(runners)
+	for i := 0; i < runners; i++ {
+		go run(&waitGroup, baton, team)
+	}
+	waitGroup.Wait()
+	pole <- team
+
+	teamWaitGroup.Done()
 }
 
 func main() {
-	var waitGroup sync.WaitGroup
-	waitGroup.Add(4)
-	baton := make(chan string, 4)
-	go receiveBaton(&waitGroup, baton, 0)
-	waitGroup.Wait()
+	teams := [...]string{
+		"The Water Coolers",
+		"The Scanners",
+		"Boss Mode",
+	}
+	pole := make(chan string, (len(teams)))
+	var teamsWaitGroup sync.WaitGroup
+	teamsWaitGroup.Add((len(teams)))
+	for i := 0; i < (len(teams)); i++ {
+		go maketeam(&teamsWaitGroup, pole, teams[i])
+	}
+	teamsWaitGroup.Wait()
+	fmt.Println("Race is over.")
+
+	fmt.Println("Podium:")
+	for i := 0; i < (len(teams)); i++ {
+		fmt.Printf("%dº: %s\n", i+1, <-pole)
+	}
 }
